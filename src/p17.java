d5370cc575a3738546fc817b9f158e20aed46336
Kim Junsik <kimjunsik333@gmail.com>
2023-07-24 22:23:44 +0900

    Java200

--- src/p17.java
+++ src/p17.java
@@ -1,2 +1,16 @@
 public class p17 {
+    class JLocation{
+        public double lat;
+        public double lng;
+
+    }
+
+    public static void main(String[] args) {
+        double lati = 37.2;
+        double longi = 24.22232;
+        JLocation jLocation = new JLocation();
+
+    }
+
+
 }
